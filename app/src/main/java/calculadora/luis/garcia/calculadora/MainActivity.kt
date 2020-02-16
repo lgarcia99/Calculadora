119e5ff105581143d716cf7970d0266cca6ef131
lgarcia99 <54911648+lgarcia99@users.noreply.github.com>
2020-02-15 23:36:22 -0700

    emergency commit

--- app/src/main/java/calculadora/luis/garcia/calculadora/MainActivity.kt
+++ app/src/main/java/calculadora/luis/garcia/calculadora/MainActivity.kt
@@ -2,18 +2,22 @@ package calculadora.luis.garcia.calculadora
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
-    val ecuacion:TextView = findViewById(R.id.ecuacion)
-    val resultado:TextView = findViewById(R.id.resultado)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // campos de texto
+        val ecuacion:TextView = findViewById(R.id.ecuacion)
+        val resultado:TextView = findViewById(R.id.resultado)
+
+        // botones numéricos
         val cero:Button = findViewById(R.id.cero) as Button;
         cero.setOnClickListener {
             ecuacion.setText(ecuacion.text.toString() + "0")
@@ -45,14 +49,65 @@ class MainActivity : AppCompatActivity() {
         }
 
         val seis:Button = findViewById(R.id.seis) as Button;
+        seis.setOnClickListener {
+            ecuacion.setText(ecuacion.text.toString() + "6")
+        }
+
         val siete:Button = findViewById(R.id.siete) as Button;
+        siete.setOnClickListener {
+            ecuacion.setText(ecuacion.text.toString() + "7")
+        }
+
         val ocho:Button = findViewById(R.id.ocho) as Button;
+        ocho.setOnClickListener {
+            ecuacion.setText(ecuacion.text.toString() + "8")
+        }
+
         val nueve:Button = findViewById(R.id.nueve) as Button;
+        nueve.setOnClickListener {
+            ecuacion.setText(ecuacion.text.toString() + "9")
+        }
+
+        // botones de operación
         val mas:Button = findViewById(R.id.mas) as Button;
+        mas.setOnClickListener {
+            ecuacion.setText(ecuacion.text.toString() + "+")
+        }
+
         val menos:Button = findViewById(R.id.menos) as Button;
+        menos.setOnClickListener {
+            ecuacion.setText(ecuacion.text.toString() + "-")
+        }
+
         val por:Button = findViewById(R.id.por) as Button;
+        por.setOnClickListener {
+            ecuacion.setText(ecuacion.text.toString() + "*")
+        }
+
         val entre:Button = findViewById(R.id.entre) as Button;
+        entre.setOnClickListener {
+            ecuacion.setText(ecuacion.text.toString() + "/")
+        }
+
         val resultB:Button = findViewById(R.id.result) as Button;
+        resultB.setOnClickListener {
+            try {
+
+            }catch (e: Exception){
+                Log.d("Exception"," message : " + e.message )
+            }
+        }
+
         val borrarB:Button = findViewById(R.id.borrar) as Button;
+        borrarB.setOnClickListener {
+            ecuacion.setText("")
+            resultado.setText("")
+        }
+    }
+
+    fun validar(texto: String): Boolean{
+        /*if () {
+
+        }*/ return true
     }
 }
