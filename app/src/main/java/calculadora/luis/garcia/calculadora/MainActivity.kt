1d00574e3449ce011c2204f3e901730f9689ffed
lgarcia99 <54911648+lgarcia99@users.noreply.github.com>
2020-02-16 17:12:39 -0700

    avance final

--- app/src/main/java/calculadora/luis/garcia/calculadora/MainActivity.kt
+++ app/src/main/java/calculadora/luis/garcia/calculadora/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import java.lang.Exception
 import java.util.regex.Pattern
 
@@ -15,114 +16,135 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // campos de texto
-        val ecuacion:TextView = findViewById(R.id.ecuacion)
-        val resultado:TextView = findViewById(R.id.resultado)
+        val ecuacion: TextView = findViewById(R.id.ecuacion)
+        val resultado: TextView = findViewById(R.id.resultado)
 
         // botones numéricos
-        val cero:Button = findViewById(R.id.cero) as Button;
-        cero.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "0")
-        }
-
-        val uno:Button = findViewById(R.id.uno) as Button;
-        uno.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "1")
-        }
-
-        val dos:Button = findViewById(R.id.dos) as Button;
-        dos.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "2")
-        }
-
-        val tres:Button = findViewById(R.id.tres) as Button;
-        tres.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "3")
-        }
-
-        val cuatro:Button = findViewById(R.id.cuatro) as Button;
-        cuatro.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "4")
-        }
-
-        val cinco:Button = findViewById(R.id.cinco) as Button;
-        cinco.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "5")
-        }
-
-        val seis:Button = findViewById(R.id.seis) as Button;
-        seis.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "6")
-        }
-
-        val siete:Button = findViewById(R.id.siete) as Button;
-        siete.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "7")
-        }
-
-        val ocho:Button = findViewById(R.id.ocho) as Button;
-        ocho.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "8")
-        }
-
-        val nueve:Button = findViewById(R.id.nueve) as Button;
-        nueve.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "9")
-        }
+        val cero: Button = findViewById(R.id.cero) as Button
+        val uno: Button = findViewById(R.id.uno) as Button
+        val dos: Button = findViewById(R.id.dos) as Button
+        val tres: Button = findViewById(R.id.tres) as Button
+        val cuatro: Button = findViewById(R.id.cuatro) as Button
+        val cinco: Button = findViewById(R.id.cinco) as Button
+        val seis: Button = findViewById(R.id.seis) as Button
+        val siete: Button = findViewById(R.id.siete) as Button
+        val ocho: Button = findViewById(R.id.ocho) as Button
+        val nueve: Button = findViewById(R.id.nueve) as Button
 
         // botones de operación
-        val mas:Button = findViewById(R.id.mas) as Button;
+        val mas: Button = findViewById(R.id.mas) as Button
+        val menos: Button = findViewById(R.id.menos) as Button
+        val por: Button = findViewById(R.id.por) as Button
+        val entre: Button = findViewById(R.id.entre) as Button
+        val resultB: Button = findViewById(R.id.result) as Button
+        val borrarB: Button = findViewById(R.id.borrar) as Button
+
+        // interacciones de la interfaz
+        cero.setOnClickListener {
+            ingresarDigito(ecuacion, "0")
+        }
+        uno.setOnClickListener {
+            ingresarDigito(ecuacion,"1")
+        }
+        dos.setOnClickListener {
+            ingresarDigito(ecuacion,"2")
+        }
+        tres.setOnClickListener {
+            ingresarDigito(ecuacion, "3")
+        }
+        cuatro.setOnClickListener {
+            ingresarDigito(ecuacion, "4")
+        }
+        cinco.setOnClickListener {
+            ingresarDigito(ecuacion, "5")
+        }
+        seis.setOnClickListener {
+            ingresarDigito(ecuacion, "6")
+        }
+        siete.setOnClickListener {
+            ingresarDigito(ecuacion, "7")
+        }
+        ocho.setOnClickListener {
+            ingresarDigito(ecuacion, "8")
+        }
+        nueve.setOnClickListener {
+            ingresarDigito(ecuacion, "9")
+        }
         mas.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "+")
+            if (validarSignos(ecuacion)) ingresarDigito(ecuacion, "+")
         }
-
-        val menos:Button = findViewById(R.id.menos) as Button;
         menos.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "-")
+            if (validarSignos(ecuacion)) ingresarDigito(ecuacion, "-")
         }
-
-        val por:Button = findViewById(R.id.por) as Button;
         por.setOnClickListener {
-            ecuacion.setText(ecuacion.text.toString() + "*")
+            if (validarSignos(ecuacion)) ingresarDigito(ecuacion, "*")
         }
-
-        val entre:Button = findViewById(R.id.entre) as Button;
         entre.setOnClickListener {
-
+            if (validarSignos(ecuacion)) ingresarDigito(ecuacion, "/")
         }
-
-        val resultB:Button = findViewById(R.id.result) as Button;
         resultB.setOnClickListener {
             try {
-
-            }catch (e: Exception){
-                Log.d("Exception"," message : " + e.message )
+                val res = validarEntrada(ecuacion)
+                when {
+                    res.isEmpty() -> throw Exception()
+                    else -> ecuacion.setText(res)
+                }
+            } catch (e: Exception) {
             }
         }
-
-        val borrarB:Button = findViewById(R.id.borrar) as Button;
         borrarB.setOnClickListener {
             ecuacion.setText("")
             resultado.setText("")
         }
     }
 
-    fun concaternar(digito: String) {
+    fun validarSignos(ecuacion: TextView): Boolean{
+        if (Pattern.matches("[/*-+]", ecuacion.text.toString())) {
+            return false;
+        }
+        return true;
+    }
+
+    fun validarEntrada(ecuacion: TextView): String {
+        val entrada = ecuacion.text.toString()
+        var split: List<CharSequence>
+        var result = ""
+
+        if(entrada.isEmpty()) {
+            Toast.makeText(this@MainActivity, "Primero debe ingresar algún valor numérico.", Toast.LENGTH_SHORT).show()
+            return result
+        }
+
+        if(esSuma(entrada)){
+            split = entrada.split("+")
+            result = (split[0].toInt() + split[1].toInt()).toString()
+        }
+        else if(esResta(entrada)){
+            split = entrada.split("-")
+            result = (split[0].toInt() + split[1].toInt()).toString()
+        }
+        else if(esSuma(entrada)){
+            split = entrada.split("+")
+            result = (split[0].toInt() + split[1].toInt()).toString()
+        }
+        else if(esDivision(entrada)) {
+            split = entrada.split("/")
+            if (split[1].toInt() == 0) {
+                Toast.makeText(this@MainActivity, "No se puede dividir por cero.", Toast.LENGTH_SHORT).show()
+                return ""
+            } else {
+                var result = split[0].toInt() / split[1].toInt()
+            }
+        }
+        return result
+    }
+
+    fun ingresarDigito(ecuacion: TextView, digito: String) {
         ecuacion.setText(ecuacion.text.toString() + digito)
     }
 
-    fun validarSignos(texto: String): Boolean{
-        if (Pattern.matches("[/*-+]", ecuacion.text.toString())) {
-            Toast.makeText(this@MainActivity, "Presione el botón \"RESULT\" antes de continuar con otra operación.", Toast.LENGTH_SHORT).show()
-            return false;
-        }
-        return true;
-    }
-
-    fun validarEntrada(texto: String): Boolean {
-        if(resultado.text.toString().isEmpty()) {
-            Toast.makeText(this@MainActivity, "Por favor, primero ingrese algún valor numérico.", Toast.LENGTH_SHORT).show()
-            return false;
-        }
-        return true;
-    }
+    fun esSuma(entrada: String): Boolean = entrada.contains("+")
+    fun esResta(entrada: String): Boolean = entrada.contains("-")
+    fun esMultiplicacion(entrada: String): Boolean = entrada.contains("*")
+    fun esDivision(entrada: String): Boolean = entrada.contains("/")
 }
